e707f4d0cbe9ad6a71b254bf40d95ad4126bade9
Ravindulakshan7 <ravindulakshanj9@gmail.com>
2026-07-01 21:02:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/velor/', // <-- repo name දාන්න
+    base: '/velor/',
+
     plugins: [react(), tailwindcss()],
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
